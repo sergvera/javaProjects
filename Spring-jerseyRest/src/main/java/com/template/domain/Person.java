b41712f512295d261644ebd47440bbf305007a47
sergvera <dhunter_12@hotmail.com>
2013-06-23 17:54:26 -0500

    Created a Spring-jerseyRes-AngularJs project (AngularJs still not implemented)

---
+++ Spring-jerseyRest/src/main/java/com/template/domain/Person.java
@@ -0,0 +1,61 @@
+package com.template.domain;
+
+public class Person
+{
+
+   private String name;
+   private String address;
+   private String hobby;
+
+   /**
+    * @return the name
+    */
+   public String getName()
+   {
+      return name;
+   }
+
+   /**
+    * @param name
+    *           the name to set
+    */
+   public void setName(String name)
+   {
+      this.name = name;
+   }
+
+   /**
+    * @return the address
+    */
+   public String getAddress()
+   {
+      return address;
+   }
+
+   /**
+    * @param address
+    *           the address to set
+    */
+   public void setAddress(String address)
+   {
+      this.address = address;
+   }
+
+   /**
+    * @return the hobby
+    */
+   public String getHobby()
+   {
+      return hobby;
+   }
+
+   /**
+    * @param hobby
+    *           the hobby to set
+    */
+   public void setHobby(String hobby)
+   {
+      this.hobby = hobby;
+   }
+
+}
---
+++ Spring-jerseyRest/src/main/java/com/template/restful/PersonService.java
@@ -0,0 +1,27 @@
+package com.template.restful;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.template.domain.Person;
+
+@Path("/person")
+public class PersonService
+{
+
+   @GET
+   @Produces({MediaType.APPLICATION_JSON})
+   @Path("dummy")
+   public Person retrievePerson()
+   {
+      Person dummy = new Person();
+      dummy.setName("Jose");
+      dummy.setAddress("House");
+      dummy.setHobby("Coding");
+
+      return dummy;
+   }
+
+}
