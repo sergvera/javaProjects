b5552299435fed5f94862ce72ef03a7abac343e8
sergvera <sergio.vera@ellucian.com>
2014-02-27 16:43:06 -0600

    First commit

---
+++ SourceTreeGitSamples/src/sourcetreegitsamples/SourceTreeGitSamples.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sourcetreegitsamples;
+
+/**
+ *
+ * @author svera
+ */
+public class SourceTreeGitSamples {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello SourceTree");
+    }
+}
